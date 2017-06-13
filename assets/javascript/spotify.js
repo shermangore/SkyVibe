13a9972572916e9d6fe0f610d4c970de17fdd83c
Jason Messer <Jason Messer>
2017-06-12 23:12:19 -0700

    spotify object to return iframe

---
+++ assets/javascript/spotify.js
@@ -0,0 +1,14 @@
+//function to return the appropriate iframe player for the input weather
+var spotify = function() {
+    return {
+        //returns sunny playlist: spotify:user:spotify:playlist:37i9dQZF1DWVlm7xgnWdvJ
+        sunny: '<iframe src="https://open.spotify.com/embed?uri=spotify%3Auser%3Aspotify%3Aplaylist%3A37i9dQZF1DWVlm7xgnWdvJ&theme=white" width="300" height="80" frameborder="0" allowtransparency="true"></iframe>',
+        //returns for a cloudy day playlist: spotify:user:1225385727:playlist:2zpkYAHiRzC6Rkmem5LCSh
+        cloudy: '<iframe src="https://open.spotify.com/embed?uri=spotify%3Auser%3A1225385727%3Aplaylist%3A2zpkYAHiRzC6Rkmem5LCSh&theme=dark" width="300" height="80" frameborder="0" allowtransparency="true"></iframe>',
+        //returns rainy day playlist: spotify:user:spotify:playlist:37i9dQZF1DXbvABJXBIyiY
+        rainy: '<iframe src="https://open.spotify.com/embed?uri=spotify%3Auser%3Aspotify%3Aplaylist%3A37i9dQZF1DXbvABJXBIyiY&theme=dark" width="300" height="80" frameborder="0" allowtransparency="true"></iframe>',
+        //returns snowy day playlist: spotify:user:11135485325:playlist:1UOKjxsR4Kcauv8oUYtNzO
+        snowy: '<iframe src="https://open.spotify.com/embed?uri=spotify%3Auser%3A11135485325%3Aplaylist%3A1UOKjxsR4Kcauv8oUYtNzO&theme=white" width="300" height="80" frameborder="0" allowtransparency="true"></iframe>'
+    }
+}();
+
