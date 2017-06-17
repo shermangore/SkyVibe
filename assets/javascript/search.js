06b3b9a7c08329d6f043a7b6a948bb84d33bd616
sherman_mortek <shermangore@gmail.com>
2017-06-17 00:22:52 -0700

    Fix for bug #9

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -8,122 +8,143 @@ function initMap(myCenter) {
         //center: {lat: 34.0593615, lng: -118.4460819},
         center: myCenter,
         zoom: 13
-});
+    });
 
-var card = document.getElementById('pac-card');
-var input = document.getElementById('pac-input');
-var types = document.getElementById('type-selector');
-var strictBounds = document.getElementById('strict-bounds-selector');
+    var card = document.getElementById('pac-card');
+    var input = document.getElementById('pac-input');
+    var types = document.getElementById('type-selector');
+    var strictBounds = document.getElementById('strict-bounds-selector');
 
-map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
 
-var autocomplete = new google.maps.places.Autocomplete(input);
+    var autocomplete = new google.maps.places.Autocomplete(input);
 
-// Bind the map's bounds (viewport) property to the autocomplete object,
-// so that the autocomplete requests use the current map bounds for the
-// bounds option in the request.
-autocomplete.bindTo('bounds', map);
+    // Bind the map's bounds (viewport) property to the autocomplete object,
+    // so that the autocomplete requests use the current map bounds for the
+    // bounds option in the request.
+    autocomplete.bindTo('bounds', map);
 
-var infowindow = new google.maps.InfoWindow();
-var infowindowContent = document.getElementById('infowindow-content');
+    var infowindow = new google.maps.InfoWindow();
+    var infowindowContent = document.getElementById('infowindow-content');
 
-infowindow.setContent(infowindowContent);
+    infowindow.setContent(infowindowContent);
 
-var marker = new google.maps.Marker({
-    map: map,
-    anchorPoint: new google.maps.Point(0, -29)
-});
+    var marker = new google.maps.Marker({
+        map: map,
+        anchorPoint: new google.maps.Point(0, -29)
+    });
 
-autocomplete.addListener('place_changed', function() {
-    infowindow.close();
-    marker.setVisible(false);
+    autocomplete.addListener('place_changed', function () {
+        infowindow.close();
+        marker.setVisible(false);
 
-    var place = autocomplete.getPlace();
+        var place = autocomplete.getPlace();
 
-    if (!place.geometry) {
-        // User entered the name of a Place that was not suggested and
-        // pressed the Enter key, or the Place Details request failed.
-        window.alert("No details available for input: '" + place.name + "'");
+        if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
 
-        return;
-    }
+            return;
+        }
 
-    // If the place has a geometry, then present it on a map.
-    if (place.geometry.viewport) {
-        map.fitBounds(place.geometry.viewport);
-        console.log(place.geometry.location.lat());
-    } else {
-        map.setCenter(place.geometry.location);
-        map.setZoom(17);  // Why 17? Because it looks good.
-    }
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+            console.log(place.geometry.location.lat());
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+        }
 
-    marker.setPosition(place.geometry.location);
-    marker.setVisible(true);
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
 
-    getWeatherCategory(place.geometry.location.lat(), place.geometry.location.lng());
+        getWeatherCategory(place.geometry.location.lat(), place.geometry.location.lng());
 
-    var address = '';
+        var address = '';
 
-    if (place.address_components) {
-        address = [
-            (place.address_components[0] && place.address_components[0].short_name || ''),
-            (place.address_components[1] && place.address_components[1].short_name || ''),
-            (place.address_components[2] && place.address_components[2].short_name || '')
-        ].join(' ');
-    }
+        if (place.address_components) {
+            address = [
+                (place.address_components[0] && place.address_components[0].short_name || ''),
+                (place.address_components[1] && place.address_components[1].short_name || ''),
+                (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+        }
 
-    infowindowContent.children['place-icon'].src = place.icon;
-    infowindowContent.children['place-name'].textContent = place.name;
-    infowindowContent.children['place-address'].textContent = address;
-    infowindow.open(map, marker);
-    $("#map").hide();
-});
+        infowindowContent.children['place-icon'].src = place.icon;
+        infowindowContent.children['place-name'].textContent = place.name;
+        infowindowContent.children['place-address'].textContent = address;
+        infowindow.open(map, marker);
+        $("#map").hide();
+    });
 
-// Sets a listener on a radio button to change the filter type on Places
-// Autocomplete.
-// function setupClickListener(id, types) {
-//     var radioButton = document.getElementById(id);
-//     radioButton.addEventListener('click', function() {
-//     autocomplete.setTypes([]);
-//     });
-// }
+    // Sets a listener on a radio button to change the filter type on Places
+    // Autocomplete.
+    // function setupClickListener(id, types) {
+    //     var radioButton = document.getElementById(id);
+    //     radioButton.addEventListener('click', function() {
+    //     autocomplete.setTypes([]);
+    //     });
+    // }
     autocomplete.setTypes([]);
 
-// setupClickListener('changetype-all', []);
-// setupClickListener('changetype-address', ['address']);
-// setupClickListener('changetype-establishment', ['establishment']);
-// setupClickListener('changetype-geocode', ['geocode']);
+    // setupClickListener('changetype-all', []);
+    // setupClickListener('changetype-address', ['address']);
+    // setupClickListener('changetype-establishment', ['establishment']);
+    // setupClickListener('changetype-geocode', ['geocode']);
 
-// document.getElementById('use-strict-bounds')
-//     .addEventListener('click', function() {
-//         console.log('Checkbox clicked! New state=' + this.checked);
-//         autocomplete.setOptions({strictBounds: this.checked});
-//     });
+    // document.getElementById('use-strict-bounds')
+    //     .addEventListener('click', function() {
+    //         console.log('Checkbox clicked! New state=' + this.checked);
+    //         autocomplete.setOptions({strictBounds: this.checked});
+    //     });
 }
 
-$(document).ready(function() {
-    $(".fa-gear").on("click", function(event) {
+$(document).ready(function () {
+    $(".fa-gear").on("click", function (event) {
         event.preventDefault();
 
         let $mapDiv = $("#map");
 
-        $mapDiv.toggle(400, "swing", function() {
-            $("#pac-card").css("display", $mapDiv.css("display"));
+        // $mapDiv.toggle(400, "swing", function () {
+        //      $("#pac-input").css("display", $mapDiv.css("display"));
 
-             if ("geolocation" in navigator) {
-                 navigator.geolocation.getCurrentPosition(function(position) {
-                     console.log(position);
-                     initMap(new google.maps.LatLng(34.0593615, -118.4460819));
-            //         initMap(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
-                 });
+        //     if ("geolocation" in navigator) {
+        //         navigator.geolocation.getCurrentPosition(function (position) {
+        //             console.log(position);
+        //             initMap(new google.maps.LatLng(34.0593615, -118.4460819));
+        //             //         initMap(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+        //         });
+        //     } else {
+        //         initMap(new google.maps.LatLng(34.0593615, -118.4460819));
+        //     }
+
+        //     // if ($mapDiv.css("display") === "none") {
+        //     //     console.log("gone");
+        //     // }
+        // });
+        
+        if ($mapDiv.css("display") === "none") {
+            var pInput = '<input id="pac-input" type="text" placeholder="Enter a location">';
+            
+            $("#pac-container").append(pInput);
+
+            $mapDiv.css("display", "block");
+            $("#pac-input").css("display", "block");
+
+            if ("geolocation" in navigator) {
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    console.log(position);
+                    initMap(new google.maps.LatLng(34.0593615, -118.4460819));
+                });
             } else {
                 initMap(new google.maps.LatLng(34.0593615, -118.4460819));
             }
-
-            // if ($mapDiv.css("display") === "none") {
-            //     console.log("gone");
-            // }
-        });
+        } else {
+            $mapDiv.css("display", "none");
+            $("#pac-input").css("display", "none");
+        }
 
         //$("#pac-card").toggle();
         //$("#infowindow-content").toggle();
